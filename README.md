bb1a2a378ce9f00391f653c109d5b94de0ad53f8
Melesio Albavera <ma6hv@umsystem.edu>
2022-01-20 22:29:29 -0600

    Now tracking cursor position and other useful information.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # TO-DO:
 **Version 0:**  
 - [X] Add canvas object.
-  - [ ] Set width and height to window size.
+  - [X] Set width and height to window size.
   - [ ] Check window dimensions for minimum size .  
 - [ ] Allow user to draw simple shapes with mouse.  
+  - [ ] Draw rectangles (two clicks from corner to corner.)
 - [ ] Calculate centroid of shape.  
+	Miscellaneous:		
+	- [X] Set cursor shape to crosshair.
+	- [X] Track cursor position.
--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 var mainCanvas = document.querySelector('canvas');
 var context = mainCanvas.getContext('2d');
+var position = [0, 0];
+var drawMode = 'Rectangle';
 mainCanvas.width = window.innerWidth;
 mainCanvas.height = window.innerHeight;
+window.addEventListener('mousemove', (event) => {position = [event.clientX, event.clientY]});
+window.addEventListener('keypress', updateDrawMode);
 
-context.font = '16px courier';
-context.fillText('DRAW MODE: ', 20, window.innerHeight-20);
+function updateDrawMode(event)
+{
+	switch(event.code)
+	{
+		case 'KeyC':
+			drawMode = 'Circle';
+			break;
+		case 'KeyL':
+			drawMode = 'Line';
+			break;
+		case 'KeyR':
+			drawMode = 'Rectangle';
+			break;
+	}
+}
+
+function init()
+{
+	window.requestAnimationFrame(draw);
+}
+
+function draw()
+{
+	context.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+	context.font = '16px courier';
+	context.fillText('CURSOR POSITION: '+' ('+position[0]+', '+position[1]+')', 20, window.innerHeight-40);
+	context.fillText('DRAW MODE: '+drawMode, 20, window.innerHeight-20);
+	window.requestAnimationFrame(draw);
+}
+
+init();
