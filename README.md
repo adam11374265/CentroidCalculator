ce5946585bc44ba805d97b7b311e9257b1dd94e5
Melesio Albavera <ma6hv@umsystem.edu>
2022-01-22 00:22:11 -0600

    Got the drawing function up and running.

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 - [X] Add canvas object.
   - [X] Set width and height to window size.
   - [ ] Check window dimensions for minimum size .  
-- [ ] Allow user to draw simple shapes with mouse.  
-  - [ ] Draw rectangles (two clicks from corner to corner.)
+- [X] Allow user to draw simple shapes with mouse.  
+  - [X] Draw rectangles (two clicks from corner to corner.)
 - [ ] Calculate centroid of shape.  
 	Miscellaneous:		
 	- [X] Set cursor shape to crosshair.
 	- [X] Track cursor position.
+        - [ ] Clear function command.
--- main.js
+++ main.js
@@ -2,13 +2,44 @@ var mainCanvas = document.querySelector('canvas');
 var context = mainCanvas.getContext('2d');
 var position = [0, 0];
 var drawMode = 'Rectangle';
+var followCursor = false;
+var shapes = [];
+var anchor0 = [0, 0];
+var anchor1 = [0, 0];
 mainCanvas.width = window.innerWidth;
 mainCanvas.height = window.innerHeight;
 window.addEventListener('mousemove', (event) => {position = [event.clientX, event.clientY]});
 window.addEventListener('keypress', updateDrawMode);
+window.addEventListener('click', updateAnchors);
+
+function updateAnchors(event)
+{
+	followCursor=!followCursor;
+	if(followCursor)
+	{
+		anchor0 = position;
+	}
+	else
+	{
+		anchor1 = position;
+		switch(drawMode)
+		{
+			case 'Line':
+				shapes[shapes.length]=['l',anchor0,anchor1];
+				break;
+			case 'Rectangle':
+				shapes[shapes.length]=['r',anchor0,anchor1];
+				break;
+			case 'Circle':
+				shapes[shapes.length]=['c',anchor0,anchor1];
+				break;
+		}
+	}
+}
 
 function updateDrawMode(event)
 {
+	followCursor = false;
 	switch(event.code)
 	{
 		case 'KeyC':
@@ -32,8 +63,55 @@ function draw()
 {
 	context.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
 	context.font = '16px courier';
-	context.fillText('CURSOR POSITION: '+' ('+position[0]+', '+position[1]+')', 20, window.innerHeight-40);
+	context.fillText('CURSOR POSITION: '+'('+position[0]+', '+position[1]+')', 20, window.innerHeight-40);
 	context.fillText('DRAW MODE: '+drawMode, 20, window.innerHeight-20);
+
+	for(let x in shapes)
+	{
+		switch(shapes[x][0])
+		{
+			case 'l':
+				context.beginPath();
+				context.moveTo(...shapes[x][1]);
+				context.lineTo(...shapes[x][2]);
+				context.stroke();
+				break;
+			case 'r':
+				context.beginPath();
+				context.rect(...shapes[x][1], shapes[x][2][0]-shapes[x][1][0], shapes[x][2][1]-shapes[x][1][1]);
+				context.stroke();
+				break;
+			case 'c':
+				context.beginPath();
+				context.arc(...shapes[x][1], ((shapes[x][2][0]-shapes[x][1][0])**2+(shapes[x][2][1]-shapes[x][1][1])**2)**(1/2), 0, 2*Math.PI)
+				context.stroke();
+				break;
+		}
+	}
+
+	if(followCursor)
+	{
+		switch(drawMode)
+		{
+			case 'Line':
+				context.beginPath();
+				context.moveTo(...anchor0);
+				context.lineTo(...position);
+				context.stroke();
+				break;
+			case 'Rectangle':
+				context.beginPath();
+				context.rect(...anchor0, position[0]-anchor0[0], position[1]-anchor0[1]);
+				context.stroke();
+				break;
+			case 'Circle':
+				context.beginPath();
+				context.arc(...anchor0, ((position[0]-anchor0[0])**2+(position[1]-anchor0[1])**2)**(1/2), 0, 2*Math.PI);
+				context.stroke();
+				break;
+		}
+	}
+
 	window.requestAnimationFrame(draw);
 }
 
